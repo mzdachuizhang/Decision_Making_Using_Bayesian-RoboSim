56be03fae8653daa2f94a01831d2666e4bfcaf74
mzdachuizhang <51194501206@stu.ecnu.edu.cn>
2021-08-27 22:20:20 +0800

    commit the existing Projects

---
+++ src/Env.java
@@ -0,0 +1,187 @@
+public class Env {
+    private String ego_lane;
+    private String rel_b0;
+    private String rel_f0;
+    private String rel_f1;
+    private String is_safe_b0;
+    private String is_safe_f0;
+    private String is_safe_f1;
+
+    // env
+    private String f0_speed;
+    private String f0_acc;
+    private String f0_lane;
+    private String f0_intension;
+    private String b0_speed;
+    private String b0_acc;
+    private String b0_lane;
+    private String b0_intension;
+    private String f1_speed;
+    private String f1_acc;
+    private String f1_lane;
+    private String f1_intension;
+    private String weather;
+
+    public Env() {
+    }
+
+    public String getEgo_lane() {
+        return ego_lane;
+    }
+
+    public void setEgo_lane(String ego_lane) {
+        this.ego_lane = ego_lane;
+    }
+
+    public String getRel_b0() {
+        return rel_b0;
+    }
+
+    public void setRel_b0(String rel_b0) {
+        this.rel_b0 = rel_b0;
+    }
+
+    public String getRel_f0() {
+        return rel_f0;
+    }
+
+    public void setRel_f0(String rel_f0) {
+        this.rel_f0 = rel_f0;
+    }
+
+    public String getRel_f1() {
+        return rel_f1;
+    }
+
+    public void setRel_f1(String rel_f1) {
+        this.rel_f1 = rel_f1;
+    }
+
+    public String getIs_safe_b0() {
+        return is_safe_b0;
+    }
+
+    public void setIs_safe_b0(String is_safe_b0) {
+        this.is_safe_b0 = is_safe_b0;
+    }
+
+    public String getIs_safe_f0() {
+        return is_safe_f0;
+    }
+
+    public void setIs_safe_f0(String is_safe_f0) {
+        this.is_safe_f0 = is_safe_f0;
+    }
+
+    public String getIs_safe_f1() {
+        return is_safe_f1;
+    }
+
+    public void setIs_safe_f1(String is_safe_f1) {
+        this.is_safe_f1 = is_safe_f1;
+    }
+
+    public String getB0_speed() {
+        return b0_speed;
+    }
+
+    public void setB0_speed(String b0_speed) {
+        this.b0_speed = b0_speed;
+    }
+
+    public String getB0_acc() {
+        return b0_acc;
+    }
+
+    public void setB0_acc(String b0_acc) {
+        this.b0_acc = b0_acc;
+    }
+
+    public String getB0_lane() {
+        return b0_lane;
+    }
+
+    public void setB0_lane(String b0_lane) {
+        this.b0_lane = b0_lane;
+    }
+
+    public String getB0_intension() {
+        return b0_intension;
+    }
+
+    public void setB0_intension(String b0_intension) {
+        this.b0_intension = b0_intension;
+    }
+
+    public String getF0_speed() {
+        return f0_speed;
+    }
+
+    public void setF0_speed(String f0_speed) {
+        this.f0_speed = f0_speed;
+    }
+
+    public String getF0_acc() {
+        return f0_acc;
+    }
+
+    public void setF0_acc(String f0_acc) {
+        this.f0_acc = f0_acc;
+    }
+
+    public String getF0_lane() {
+        return f0_lane;
+    }
+
+    public void setF0_lane(String f0_lane) {
+        this.f0_lane = f0_lane;
+    }
+
+    public String getF0_intension() {
+        return f0_intension;
+    }
+
+    public void setF0_intension(String f0_intension) {
+        this.f0_intension = f0_intension;
+    }
+
+    public String getF1_speed() {
+        return f1_speed;
+    }
+
+    public void setF1_speed(String f1_speed) {
+        this.f1_speed = f1_speed;
+    }
+
+    public String getF1_acc() {
+        return f1_acc;
+    }
+
+    public void setF1_acc(String f1_acc) {
+        this.f1_acc = f1_acc;
+    }
+
+    public String getF1_lane() {
+        return f1_lane;
+    }
+
+    public void setF1_lane(String f1_lane) {
+        this.f1_lane = f1_lane;
+    }
+
+    public String getF1_intension() {
+        return f1_intension;
+    }
+
+    public void setF1_intension(String f1_intension) {
+        this.f1_intension = f1_intension;
+    }
+
+    public String getWeather() {
+        return weather;
+    }
+
+    public void setWeather(String weather) {
+        this.weather = weather;
+    }
+}
